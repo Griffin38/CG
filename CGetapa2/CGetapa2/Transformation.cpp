7b04789807238a914d36df3f97d380e50cdbf41b
Griffin38 <pedrolima.0000@gmail.com>
2016-04-03 19:45:03 +0100

    add color

--- CGetapa2/CGetapa2/Transformation.cpp
+++ CGetapa2/CGetapa2/Transformation.cpp
@@ -3,18 +3,20 @@
 Transformation::Transformation() {
 	transl = Type::Type();
 	rot = Type::Type();
-	scale = Type::Type();
+	scale = Type::Type(1,1,1,1);
+	color = Type::Type();
 }
 
-Transformation::Transformation(Type t, Type r, Type e) {
+Transformation::Transformation(Type t, Type r, Type e,Type f) {
 	transl = t;
 	rot = r;
 	scale = e;
+	color = f;
 }
 
 bool Transformation::trasnformacaoVazia() {
 	bool res = false;
-	if (transl.tipoVazio() && rot.tipoVazio() && scale.tipoVazio())
+	if (transl.tipoVazio() && rot.tipoVazio() && scale.tipoVazio() & color.tipoVazio())
 		res = true;
 
 	return res;
--- CGetapa2/CGetapa2/Transformation.h
+++ CGetapa2/CGetapa2/Transformation.h
@@ -5,15 +5,19 @@ class Transformation {
 	Type transl;
 	Type rot;
 	Type scale;
+	Type color;
 public:
 	Transformation();
-	Transformation(Type, Type, Type);
+	Transformation(Type, Type, Type,Type);
 	Type getTranslacao() { return transl; }
 	Type getRotacao() { return rot; }
 	Type getEscala() { return scale; }
+	Type getCor() { return color; }
 	void setTranslacao(Type t) { transl = t; }
-	void setRotacao(Type t) { rot = t; }
+	void setCor(Type t) { color = t; }
 	void setEscala(Type t) { scale = t; }
+	void setRotacao(Type t) { rot = t; }
+	
 	bool trasnformacaoVazia();
 	~Transformation(void);
 }; 
